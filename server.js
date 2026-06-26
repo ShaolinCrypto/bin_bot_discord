dfe07ec20abcbc4ce3e6ae1004f630d278d954fd
ShaolinCrypto <36669127+ShaolinCrypto@users.noreply.github.com>
2026-06-26 23:03:46 +0100

    Update server.js
    Change port to 8080

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.raw({ type: "*/*" }));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const DISCORD_PING = 1;
 const APPLICATION_COMMAND = 2;
